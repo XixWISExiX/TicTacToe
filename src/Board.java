7de3115cc924d0361930165758d029c55e43b219
Joshua Wiseman <addalooptoit@gmail.com>
2022-02-22 20:35:57 -0600

    Working board changer method

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,11 @@
 import java.util.Arrays;
 
 public class Board {
-	private char[] boardValues;
+	private char[] boardPos;
 	private char[][] board;
 	
 	public Board() {
-//		board = "___|___|___" + System.lineSeparator();
-//		         ___|___|___
-//				 ___|___|___
-//		            |   |
-//		
-//		 x | o |
-//		---+---+---
-//		   |   |
-//		---+---+---
-//		   |   |
-//		
-//		
-//		
-		
-		boardValues = new char[9];
+		boardPos = new char[9];
 		char[][] boardHolder = { {' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '},
 				  {'-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-'},
 				  {' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '},
@@ -28,12 +14,30 @@ public class Board {
 		board = boardHolder;
 	}
 	
+	public void boardChanger(int pos, char player) {
+		switch (pos) {
+			case 1: board[0][1] = player;
+				break;
+			case 2: board[0][5] = player;
+				break;
+			case 3: board[0][9] = player;
+				break;
+			case 4: board[2][1] = player;
+				break;
+			case 5: board[2][5] = player;
+				break;
+			case 6: board[2][9] = player;
+				break;
+			case 7: board[4][1] = player;
+				break;
+			case 8: board[4][5] = player;
+				break;
+			case 9: board[4][9] = player;
+				break;
+		}
+	}
+	
 	public String toString() {
-//		for(int i = 0; i < board.length; ++i) {
-//			System.out.println(board[i]);
-//		}
-//		return Arrays.deepToString(board);
-		
 		String table = "";
 		for(int i = 0; i < board.length; ++i) {
 			for(int k = 0; k < board[0].length; ++k) {
--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,28 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Board b = new Board();
-		System.out.println(b);
+		Board board = new Board();
+		Scanner scnr = new Scanner(System.in);
+		String brake = "";
+		int[] values = new int[9];
+		
+		System.out.println("Welcome to tic tac toe!\n");
+		System.out.println("Do you want to be x or o? (enter x or o)\n");
+		char player = scnr.next().charAt(0);
+		while(!brake.equals("exit")) {
+			System.out.println("\n" + board + "\n");
+			
+			System.out.println("Please enter your position which you want to choose.");
+			System.out.println("(enter 1-9)");
+			int pos = scnr.nextInt();
+			
+			// if that position has a space, then enter in the players position
+			if() {
+				
+			}
+		}
 	}
-
 }
